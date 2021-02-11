c64172fdea930dfef52dd55fcaf94e87270026ff
Andrew Shannon <andrew@andopen.co>
2021-02-11 09:40:50 +0000

    success error

--- main.js
+++ main.js
@@ -1,5 +1,74 @@
 gsap.registerPlugin(ScrollTrigger);
 
+
+
+//Main animation
+let dots = [],
+  bg = document.querySelector('#featureAnimation'),
+  i,
+  dot
+
+const shapes = [
+  "shape",
+  "shape"
+]
+
+const images = [
+    'flower_pot.png',
+    'donkey.png',
+  'trowel.png',
+  'big_read.png',
+  'city_print.png',
+  'sock.png',
+  'sock.png'
+]
+
+
+  // create 80 dot elements and put them in an array
+for (i = 0; i < 30; i++) {
+  dot = document.createElement('div')
+  dot.setAttribute('class', "shape")
+  bg.appendChild(dot)
+  dots.push(dot)
+}
+
+  // assign random background images to dots
+const setBackgroundImages = function () {
+  dots.forEach (dot => {
+const random = Math.floor(Math.random() * (0, images.length));
+dot.style.backgroundImage = 'url(' + images[random] + ')';
+  })
+
+}
+setBackgroundImages()
+
+//set the initial position of all the dots, and pick a random color for each from an array of colors
+gsap.set(dots, {
+  scale: 'random(3, 3)',
+  x: '50vw',
+  y: '100vh',
+  rotation: 'random(0, 360)'
+})
+
+// create the physics2D animation
+   let timeLine = gsap.to(dots, {
+  duration: 3.5,
+  physics2D: {
+    velocity: 'random(350, 400)',
+    angle: "random(260, 280)",
+    gravity: 'random(250, 300)',
+    friction: 'random(0.05, 0.04)',
+  },
+   repeat: -1,
+})
+
+
+
+
+const index = document.getElementById('index')
+
+if(index){
+
 // Insta
 // ScrollTrigger.defaults({
 //   markers: true,
@@ -42,72 +111,6 @@ ScrollTrigger.create({
   trigger: ".wins",
   start: "-20%",
 });
-
-//Main animation
-let dots = [],
-  bg = document.querySelector('#featureAnimation'),
-  i,
-  dot
-
-const shapes = [
-  "shape",
-  "shape"
-]
-
-const images = [
-    'flower_pot.png',
-    'donkey.png',
-  'trowel.png',
-  'big_read.png',
-  'city_print.png',
-  'sock.png',
-  'sock.png'
-]
-
-const index = document.getElementById('index')
-const success = document.getElementById('success')
-
-
-if(index, success) {
-  // create 80 dot elements and put them in an array
-for (i = 0; i < 30; i++) {
-  dot = document.createElement('div')
-  dot.setAttribute('class', "shape")
-  bg.appendChild(dot)
-  dots.push(dot)
-}
-
-  // assign random background images to dots
-const setBackgroundImages = function () {
-  dots.forEach (dot => {
-const random = Math.floor(Math.random() * (0, images.length));
-dot.style.backgroundImage = 'url(' + images[random] + ')';
-  })
-
-}
-setBackgroundImages()
-
-//set the initial position of all the dots, and pick a random color for each from an array of colors
-gsap.set(dots, {
-  scale: 'random(3, 3)',
-  x: '50vw',
-  y: '100vh',
-  rotation: 'random(0, 360)'
-})
-
-// create the physics2D animation
-   let timeLine = gsap.to(dots, {
-  duration: 3.5,
-  physics2D: {
-    velocity: 'random(350, 400)',
-    angle: "random(260, 280)",
-    gravity: 'random(250, 300)',
-    friction: 'random(0.05, 0.04)',
-  },
-   repeat: -1,
-})
-
-}
   
 
 //Marquee
@@ -182,18 +185,4 @@ addressTwoTag.addEventListener('input', evt => {
 checkValue()
 })
 
-
-
-// gsap.to('.card', {
-//   rotateX:360,
-//   duration: 1,
-// })
-
-// const tlTestTwo = gsap.timeline();
-// tlTestTwo.staggerTo('.card', 0.5, {rotateX:360, duration: 0.5, ease:"none"}, 0.1)
-
-// const tlTestTwo = gsap.timeline().delay(4);
-
-// tlTestTwo.to('.card .back', {rotateX:180, duration: 0.5, ease:"none"})
-// .to('.card .front', {rotateX:360, duration: 0.5, ease:"none"}, 0)
-// .to('.card', {rotateX:360, duration: 0.5, ease:"none"}, 0)
+}
